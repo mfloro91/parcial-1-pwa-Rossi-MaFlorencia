efa36ec8f44ff834aff4289f4fde4a6c0ff6fd86
Flor Rossi <mfloro.91@gmail.com>
2024-04-27 12:12:49 -0300

    Feat: agrego pagina detalle

--- script.js
+++ script.js
@@ -9,38 +9,38 @@ idPokemones = [];
 // Hago una función que consulta con fetch datos básicos de 100 pokemones
 
 const requestPokemons = () => {
-    
+
     // Voy a la URL general y me traigo nombre del pokemon
 
-    fetch(urlPokemon) 
-    .then (respuesta => respuesta.json())
-    .then(respuesta => {
-        const resultados = respuesta.results;
+    fetch(urlPokemon)
+        .then(respuesta => respuesta.json())
+        .then(respuesta => {
+            const resultados = respuesta.results;
 
-        // Renderizo respuestas usando la función mostrarCard
-        // Me guardo URL en array 
+            // Renderizo respuestas usando la función mostrarCard
+            // Me guardo URL en array 
 
-        resultados.forEach(element => {
-            const id = (element["url"].slice(34, -1));
-            
-            //idPokemones.push(id);
+            resultados.forEach(element => {
+                const id = (element["url"].slice(34, -1));
+
+                //idPokemones.push(id);
 
                 fetch(urlPokemonPorId + id)
-                .then (respuestaDetalles => respuestaDetalles.json())
-                .then (respuestaDetalles => {
-                    const resultadosDetalles = respuestaDetalles;
-                    //console.log(resultadosDetalles);
-                    const habilidad = resultadosDetalles[`abilities`][0][`ability`][`name`];
-                    const img = resultadosDetalles[`sprites`][`front_shiny`];
-                    console.log(img);
-                    mostrarCard(element, img, habilidad);
+                    .then(respuestaDetalles => respuestaDetalles.json())
+                    .then(respuestaDetalles => {
+                        const resultadosDetalles = respuestaDetalles;
+                        //console.log(resultadosDetalles);
+                        const habilidad = resultadosDetalles[`abilities`][0][`ability`][`name`];
+                        const img = resultadosDetalles[`sprites`][`front_shiny`];
+                        console.log(img);
+                        mostrarCard(element, img, habilidad);
 
-                });          
-        
+                    });
+
+            });
         });
-    });
 
-}    
+}
 
 requestPokemons();
 
@@ -50,11 +50,11 @@ const mostrarCard = (pokemon, img, habilidad) => {
     let contenedorCards = document.querySelector(".cardContainer");
 
     let contenedorCol = document.createElement("div");
-    contenedorCol.setAttribute("class", "col-12 col-md-6 col-lg-3 p-0");
+    contenedorCol.setAttribute("class", "col-12 col-md-6 col-lg-4 col-xl-3 p-1");
 
     let contenedorCard = document.createElement("div");
     contenedorCard.setAttribute("class", "card cardPoke");
-    contenedorCard.setAttribute("style", "width: 20rem;");
+    contenedorCard.setAttribute("style", "width: 18rem;");
 
     let divCardBody = document.createElement("div");
     divCardBody.setAttribute("class", "card-body");
@@ -66,12 +66,12 @@ const mostrarCard = (pokemon, img, habilidad) => {
     nombrePoke.setAttribute("class", "card-text col-9 h3");
     nombrePoke.innerText = `${pokemon.name}`
 
-    
+
     let idPoke = document.createElement("p");
     idPoke.setAttribute("class", "card-text col-3 h5");
     idPoke.innerText = `#${pokemon.url.slice(34, -1)}`;
 
-    
+
     let imgPoke = document.createElement("img");
     imgPoke.setAttribute(`src`, `${img}`);
     imgPoke.setAttribute(`class`, `imgCard`);
@@ -81,17 +81,26 @@ const mostrarCard = (pokemon, img, habilidad) => {
     habilidadPoke.setAttribute("class", "card-text col-12 h6");
     habilidadPoke.innerText = `Habilidad: ${habilidad}`;
 
+    let link = document.createElement("a");
+    link.setAttribute("href", "pokePorId.html");
+    link.setAttribute("class", "d-grid gap-2");
+
     let button = document.createElement("button");
-    button.setAttribute("class", "btn btn-primary")
+    button.setAttribute("class", "btn btn-primary");
     button.innerText = "Ver más";
 
     divRow.append(nombrePoke, idPoke, imgPoke, habilidadPoke);
     divCardBody.append(divRow);
-    contenedorCard.append(divCardBody, button);
+    link.append(button);
+    contenedorCard.append(divCardBody, link);
     contenedorCol.append(contenedorCard);
     contenedorCards.append(contenedorCol);
 
 }
+
+
+
+
 
 
 /*            <div class="card cardPoke me-2 mb-2" style="width: 20rem;">
